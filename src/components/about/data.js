f1df6898108afc0fe399f0ae4ea130f47ec69145
anhgit-91 <tadang.developer@gmail.com>
2023-10-27 20:36:07 +1100

    URL changed

--- src/components/about/data.js
+++ src/components/about/data.js
@@ -2,62 +2,62 @@ const items = [
     {
         id: 1,
         title: "HTML",
-        img: "/icons/html-5.png",
+        img: "./icons/html-5.png",
     },
     {
         id: 2,
         title: "CSS",
-        img: "/icons/css.png",
+        img: "./icons/css.png",
     },
     {
         id: 3,
         title: "JavaScript",
-        img: "/icons/js.png",
+        img: "./icons/js.png",
     },
     {
         id: 4,
         title: "React",
-        img: "/icons/react.png",
+        img: "./icons/react.png",
     },
     {
         id: 5,
         title: "jQuery",
-        img: "/icons/jquery.png",
+        img: "./icons/jquery.png",
     },
     {
         id: 6,
         title: "SASS",
-        img: "/icons/sass.png",
+        img: "./icons/sass.png",
     },
     {
         id: 7,
         title: "Bootstrap",
-        img: "/icons/bootstrap.png",
+        img: "./icons/bootstrap.png",
     },
     {
         id: 8,
         title: "Tailwind",
-        img: "/icons/tailwind.png",
+        img: "./icons/tailwind.png",
     },
     {
         id: 9,
         title: "Responsive",
-        img: "/icons/responsive.png",
+        img: "./icons/responsive.png",
     },
     {
         id: 10,
         title: "Mysql",
-        img: "/icons/mysql.png",
+        img: "./icons/mysql.png",
     },
     {
         id: 11,
         title: "Visual Studio Code",
-        img: "/icons/vscode.png",
+        img: "./icons/vscode.png",
     },
     {
         id: 12,
         title: "Github",
-        img: "/icons/github.png",
+        img: "./icons/github.png",
     },
 ];
 
