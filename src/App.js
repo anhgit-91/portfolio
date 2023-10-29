979eca915e4d2c562eb770b9fa2b4fdcda9d4b8a
anhgit-91 <tadang.developer@gmail.com>
2023-10-29 15:45:18 +1100

    New - image slider function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import Hero from "./components/hero/Hero";
+import Cursor from "./components/cursor/Cursor";
 import Navbar from "./components/navbar/Navbar";
 import About from "./components/about/About";
 import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
 import "./App.scss";
-import Cursor from "./components/cursor/Cursor";
 
 function App() {
     return (
         <div className="App">
-            <Cursor />
-            <section id="Homepage">
+            {/* <Cursor /> */}
+            {/* <section id="Homepage">
                 <Navbar />
                 <Hero />
             </section>
             <section id="About">
                 <About />
-            </section>
+            </section> */}
             <Projects />
-            <section id="Contact">
+            {/* <section id="Contact">
                 <Contact />
-            </section>
+            </section> */}
         </div>
     );
 }
--- src/components/projects/data.js
+++ src/components/projects/data.js
@@ -14,7 +14,11 @@ const items = [
     {
         id: 3,
         title: "Boostrap Project",
-        img: "https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&w=600",
+        img: [
+            "https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&w=600",
+            "https://images.pexels.com/photos/1387037/pexels-photo-1387037.jpeg?auto=compress&cs=tinysrgb&w=600",
+            "https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&w=600",
+        ],
         desc: "Quaerat voluptate mollitia illo dolorem accusantium eligendi corrupti quis aperiam incidunt magni nam ipsam,",
     },
 ];
